77cfb7ab14404bcaf1f7c4d27ff84ee9dd10b861
Bob Long <robertjflong@gmail.com>
2015-09-16 10:25:41 +0100

    first

---
+++ run.rb
@@ -0,0 +1,48 @@
+require 'rufus-scheduler'
+require 'mail'
+
+puts "I will run shelduck periodically. Please feel free to kill me, but please restart me!"
+
+options = { :address              => "smtp.gmail.com",
+            :port                 => 587,
+            :domain               => 'smtp.gmail.com',
+            :user_name            => ENV['shelduck_email_username'],
+            :password             => ENV['shelduck_email_password'],
+            :authentication       => 'plain',
+            :enable_starttls_auto => true  }
+
+Mail.defaults do
+  delivery_method :smtp, options
+end
+
+def success?(x)
+  !x.include?('Bad topic')
+end
+
+def mail(s)
+  Mail.deliver do
+       to ENV['shelduck_email_recipient']
+     from "#{ENV['shelduck_email_username']}@gmail.com"
+  subject 'shelduck results' + (success?(s) ? '' : ' [failure]')
+     body s
+  end
+end
+
+scheduler = Rufus::Scheduler.new
+
+scheduler.interval '30m' do
+  puts "Running!"
+  result = nil
+  2.times do
+    result = `cd ~/scalpel-master && stack exec scalpel intercom`
+    break if success?(result)
+    puts "Unsuccessful run"
+  end
+  result.gsub!(/\[0m/, '')
+  result.gsub!(/\[3.m/, '')
+  result.gsub!(/\[38;5;.m/, '')
+  mail result
+  puts "Finished!"
+end
+
+scheduler.join
